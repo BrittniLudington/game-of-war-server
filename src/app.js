215395ee350df72adbf15f0cf97681d1b7c9cb97
BrittniLudington <brittniludington@gmail.com>
2019-05-11 18:31:26 -0700

    testing get users

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const {NODE_ENV} = require('./config');
+const {NODE_ENV,DATABASE_URL} = require('./config');
+const {Client} = require('pg');
+const parser = require('body-parser');
+
 
 const app = express();
 
@@ -12,11 +15,39 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+app.use(parser.json());
+
+const client = new Client({
+    connectionString: DATABASE_URL,
+    ssl: true
+})
+client.connect((err) =>
+{
+    if(err)
+    {
+        console.log(err);
+    }
+});
 
 app.get('/', (req,res) =>
 {
-    res.send('Hello, World!');
+    res.send('users for users, games for games');
 })
+
+app.get('/users',(req,resApp) =>
+{
+    //client.connect();
+    client.query("SELECT * from users;", (err,res) =>
+    {
+        if(err) throw err;
+        for(let row of res.rows)
+        {
+            console.log(JSON.stringify(row));
+        }
+        resApp.json(res.rows);
+    })
+})
+
 
 app.use(function errorHandler(error,req,res,next)
 {
