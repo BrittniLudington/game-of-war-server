c5bfce8245bec2c75a63dc4683de71778219b2f7
BrittniLudington <brittniludington@gmail.com>
2019-05-12 18:09:49 -0700

    typo

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ app.get('/files',(req,resApp) =>
     })
 })
 
-/*app.push('/files',(req,resApp) =>
+app.post('/files',(req,resApp) =>
 {
     client.query(`INSERT INTO files values(${req.body.username},${req.body.date},0,0,0%,null)`,(err,res) =>
     {
@@ -61,7 +61,7 @@ app.get('/files',(req,resApp) =>
     });
 })
 
-*/
+
 
 app.get('/games',(req,resApp) =>
 {
