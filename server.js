002dbb00eb76640147505d23122d5eabd295f4f1
BrittniLudington <brittniludington@gmail.com>
2019-05-15 22:17:04 -0700

    modified post endpoints

--- server.js
+++ server.js
@@ -42,25 +42,63 @@ app.get('/files',(req,resApp) =>
     client.query("SELECT * from files;", (err,res) =>
     {
         if(err) throw err;
-        for(let row of res.rows)
-        {
-            console.log(JSON.stringify(row));
-        }
+
         resApp.json(res.rows);
     })
 })
 
+app.delete('/user/:name',(req,resApp) =>
+{
+    let deckTable = req.params.name + "table";
+    //console.log(req.params);
+    // first delete deck and game
+    client.query(`DELETE from games where gameid = (SELECT gameid from files where username = '${req.params.name}')`,(err) => {if(err) throw err;});
+    client.query(`DROP TABLE IF EXISTS ${deckTable}`,(err) => {if(err) throw err;});
+    client.query(`DELETE from files where username = '${req.params.name}'`,(err) => {if(err) throw err;});
+    
+})
+
 app.post('/files',(req,resApp) =>
 {
-    client.query(`INSERT INTO files values(${req.body.username},${req.body.date},0,0,0%,null)`,(err,res) =>
+    client.query(`SELECT MAX(gameid) FROM files`, (err,resOne) =>
     {
-        if(err) 
-            {
-                console.log(err.stack);
-                throw err;
-            }
-        
-    });
+        if(err) throw err;
+        let id = resOne.rows[0].max + 1;
+        client.query(`INSERT INTO files values('${req.body.username}',NOW(),0,0,'0%',${id});`,(err,res) =>
+        {
+            if(err) 
+                {
+                    console.log('HI',err.stack);
+                    throw err;
+                }
+            
+        })
+        client.query(`INSERT INTO games values(${id},0,0,0)`)
+        let deckTable = req.body.username + "table";
+        client.query(`CREATE TABLE ${deckTable} AS TABLE deckoriginal;`,(err,result) =>
+        {
+            if(err) throw err;
+        })
+    })
+   
+})
+
+app.put('/files',(req,resApp) =>
+{
+    /* For any change in gameid 
+    Cases:
+        - new game id must be one higher than current highest
+        - If no games exists, have it be zero
+        */
+    client.query(`SELECT MAX(gameid) FROM files`,(err,res) =>
+    {
+        if(err)
+        {
+            throw err;
+        }
+        console.log(res);
+    })
+
 })
 
 
