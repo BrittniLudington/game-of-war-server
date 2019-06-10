d7bf9ff5b8ee9fa16865f0f79b3dc052365a601d
BrittniLudington <brittniludington@gmail.com>
2019-06-10 13:50:02 -0700

    added header

--- src/server.js
+++ src/server.js
@@ -191,8 +191,9 @@ app.use(function errorHandler(error,req,res,next)
         response = {message: error.message, error};
     }
     res.setHeader('Access-Control-Allow-Origin','*');
-    
-    res.status(500).json(response);
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    next();
+   // res.status(500).json(response);
 })
 
 
